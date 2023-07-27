1bc5440fadce827e679b4b7189b01f79f56405ae
evabth <evanhild@verizon.net>
2023-07-27 19:49:40 -0400

    Header of the Website
    added a header with buttons to navigate the website and title

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
 import './App.css';
 import React from 'react';
+import menu from './menu.png';
+import email from './email.png';
 
 
 function App() {
   return (
-    React.createElement(
-      "div",
-      {className : "App"},
-      React.createElement(
-        "div",
-        {className: "Page"},
-        'this is the header'
-      )
-    
-    )
+    <div className='Page'>
+      <div className='Header'>
+        <div className='Header-item'>
+          <button className='Menu'><img className = 'Picture' src ={menu} alt = 'cannot be found' onClick={optionsMenu}/></button>
+        </div>
+        <div className='Header-item Title'>
+          Evan Hild
+        </div>
+        <div className='Header-item'>
+          <button className='Button'><img className='Picture' src ={email} alt= 'email' /></button>
+        </div>
+        
+        
+      </div>
+    </div>
     
   );
+}
+
+let counter= 0;
+
+const optionsMenu = function (){
+  console.log(`hello ${counter++}`)
 }
 
 export default App;
