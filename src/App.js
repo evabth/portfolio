b9542c1789dcb4fc9f22a5e83772147a761b1988
evabth <evanhild@verizon.net>
2023-07-28 17:40:00 -0400

    Improve button functionality
    buttons better

--- src/App.js
+++ src/App.js
@@ -2,20 +2,21 @@ import './App.css';
 import React from 'react';
 import menu from './menu.png';
 import email from './email.png';
-
+import menuDarker from './menudarker.png';
+import emailDarker from './emaildarker.png';
 
 function App() {
   return (
     <div className='Page'>
       <div className='Header'>
         <div className='Header-item'>
-          <button className='Menu'><img className = 'Picture' src ={menu} alt = 'cannot be found' onClick={optionsMenu}/></button>
+          <button className='Menu'><img className = 'Picture' src ={menu} alt = 'cannot be found' onClick={optionsMenu} onMouseOver={(event) => menuOn(event.target)} onMouseOut={(event) => menuOff(event.target)}/></button>
         </div>
         <div className='Header-item Title'>
           Evan Hild
         </div>
         <div className='Header-item'>
-          <button className='Button'><img className='Picture' src ={email} alt= 'email' /></button>
+          <button className='Button'><img className='Picture' src ={email} alt= 'email' onMouseOver={(event) => emailOn(event.target)} onMouseOut={(event) => emailOff(event.target)}/></button>
         </div>
         
         
@@ -29,6 +30,22 @@ let counter= 0;
 
 const optionsMenu = function (){
   console.log(`hello ${counter++}`)
+}
+
+const menuOn = function (image){
+  image.src = menuDarker
+}
+
+const menuOff = function (image){
+  image.src = menu
+}
+
+const emailOn = function (image){
+  image.src = emailDarker
+}
+
+const emailOff = function (image){
+  image.src = email
 }
 
 export default App;
