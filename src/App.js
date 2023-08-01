4c9a998dc77025148e653c094138268db89d8aa7
evabth <evanhild@verizon.net>
2023-08-01 18:19:13 -0400

    Links to New Pages
    added multiple page capability

--- src/App.js
+++ src/App.js
@@ -1,37 +1,11 @@
 import './App.css';
 import React from 'react';
-import menu from './menu.png';
-import email from './email.png';
-import menuDarker from './menudarker.png';
-import emailDarker from './emaildarker.png';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className='Page'>
-      <div className='Header'>
-        <div className='Header-item'>
-          <button className='Menu'>
-            <img className = 'Picture' src ={menu} alt = 'cannot be found' onClick={optionsMenu} onMouseOver={(event) => menuOn(event.target)} onMouseOut={(event) => menuOff(event.target) }/>
-            <div className='dropdown-content' id = 'dropdown'>
-              <a href="#home">Home</a>
-              <a href="#about">About</a>
-              <a href="#Projects">Projects</a>
-              <a href="#music">Music</a>
-            </div>
-          </button>
-        </div>
-        <div className='Header-item Title'>
-          Evan Hild
-        </div>
-        <div className='Header-item'>
-          <button className='email'>
-            <img className='Picture' src ={email} alt= 'email' onMouseOver={(event) => emailOn(event.target)} onMouseOut={(event) => emailOff(event.target)}/>
-            
-          </button>
-        </div>
-        
-        
-      </div>
+      <Navbar/>
     </div>
     
   );
@@ -39,27 +13,6 @@ function App() {
 
 
 
-const optionsMenu = function (){
-  let dropdown = document.getElementById('dropdown')
 
-  dropdown.classList.toggle('show')
-
-}
-
-const menuOn = function (image){
-  image.src = menuDarker
-}
-
-const menuOff = function (image){
-  image.src = menu
-}
-
-const emailOn = function (image){
-  image.src = emailDarker
-}
-
-const emailOff = function (image){
-  image.src = email
-}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,13 +2,38 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Projects from './Pages/Projects';
 import reportWebVitals from './reportWebVitals';
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App/>,
+  },
+  {
+    path: '/Projects',
+    element : <Projects/>,
+  },
+  {
+    path : '/Music',
+    element: <div>hello</div>
+  },
+  {
+    path : '/About',
+    element: <div>about</div>
+  },
+  
+
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <RouterProvider router = {router}/>
 );
 
 // If you want to start measuring performance in your app, pass a function
