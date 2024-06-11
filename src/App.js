76b476c885f43d5c6fd04c42727336fd3f4b9ff4
evabth <evanhild@verizon.net>
2024-06-11 16:17:54 -0400

    Replaced picture with link and change favicon

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import React from 'react';
 import Navbar from './Components/Navbar';
-import icon from './Components/IMG-5056.jpg';
 
 function App() {
   return (
@@ -9,7 +8,7 @@ function App() {
       <Navbar/>
       <div class = 'Content'>
         <div className='Image-Text-Container'>
-          <img src={icon} alt ='Image of Evan Hild (Image could not be loaded)' />
+          <img src="https://i.imgur.com/U313Pn5.jpg" alt ='Image of Evan Hild (Image could not be loaded)' />
           <p>Hello my name is Evan Hild. I am currently studying computer science & economics 
             at the University of Maryland. I am currently in my sophomore year and I plan to graduate 
             in 2026. Please feel free to explore around my website. I have projects that I am working on, 
